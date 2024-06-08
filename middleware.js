a26f2eb24c88a9b778d7e85cd0a30e3027bb7343
TrustSign123 <developer.trustsign@gmail.com>
2024-06-08 22:44:43 +0530

    update

--- middleware.js
+++ middleware.js
@@ -1,8 +1,13 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware({
-  publicRoutes: ["/api/webhooks/(.*)"],
-});
+export default clerkMiddleware(
+  {
+    publicRoutes: ["/api/webhook"],
+  },
+  { debug: true }
+);
+
+// console.log("middleware...");
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
